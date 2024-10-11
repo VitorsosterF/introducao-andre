28ee87dcccf39c6cf02ba7a47972ed499fafc780
VitorsosterF <vitor.soster.f@gmail.com>
2024-10-11 15:23:25 -0300

    Quinto commit

--- atividade-github.py
+++ atividade-github.py
@@ -1,3 +1,5 @@
 print("Este é um código para a atividade do professor André")
 numero1 = 5 + 5
 print("Esta atividade tem que ter 10 linhas e commits para cada uma")
+print("Linha número 4 do código!")
+print("Esta linha vai para o quinto commit")
